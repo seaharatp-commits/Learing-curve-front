b0626070d40c71390fe63a7bd812be4fd4d1e620
Peaw <seaharat.p@gmail.com>
2026-07-10 16:05:25 +0700

    เพิ่มการสนับสนุนการสร้าง standalone โดยการคัดลอกไฟล์ .next/static และ public/ ในขั้นตอนการสร้าง

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   reactStrictMode: true,
-  transpilePackages: ["@heroui/react"],
+  allowedDevOrigins: [
+    "localhost",
+    "https://did.smartalliance.co.th",
+    "did.smartalliance.co.th",
+    "localhost:3001",
+    "192.168.33.162",
+    "192.168.33.162:3001",
+    "192.168.33.166",
+    "192.168.33.160",
+    "192.168.33.166:3000",
+    "192.168.33.160:6601",
+    "192.168.33.42",
+    "154.197.124.206",
+  ],
 };
-
 export default nextConfig;
